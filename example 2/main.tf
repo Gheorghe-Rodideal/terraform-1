d45c69ac91570d8d00a91ae249a7e717fda295b2
Islam Kambarov <ikambarov@yahoo.com>
2020-11-06 02:40:50 +0000

    Updated key

--- example 2/main.tf
+++ example 2/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "us-east-1-bastion-key" {
-  key_name   = "bastion-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkzQCcChKFycIWNj/XHB8pli/ar1U6UWT24MxO2q46l5CJ0nZ2sG612R511RojA3UTNRWCpDdolXHpaONvuWU3HVVX/WIVtqGlxwdPN1BF6zREY9OuO/CONqwo6hYCLCGwlI3WNSXCAjHA1Cztne42fy7nzUNVZQ0kDdawg8Ud7jt0rWYqsbZce6SChrB6mmPRpQG8wyk5jgaGPK1kjRMCBp2pHvgVK39NG769XGwaEfVatiiVsF9nLbU+iL2WgkcOcvzdBXiTNU72qUIw2rdIemAmSLSQO/okkhJLf5bgF+JeQAjGxkyMouHNk0MaiWkbiRyICbOnzd8+y52hzPI/ ec2-user@Bastion"
+  key_name   = "bastion"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUOM4jjcu0j1hU4xzbpwgNmnlMP4ta0RQhysqMovbo7su8V6tijBPUBrYcYDv2w4yuAk70VbYHYTD8WSmDSEwy8/t3dpJfIu//ISbCA8P/p8Kgf2yfIkO2fJen4LA56/jnqn3GClcVfK9fzwrGYP/ocfoCScpf5j9uGIoZl55BFrKwQ/7H03KfuFoBEZcpPCpI6w9pX6lGe7Od3lqu2O7ezQztUpfwAZdLMqxHa5704HvqnbYSJkWj68SSNe8J3jV3/d/pFjBgF+cPp9zQVliJCepDr1jiS6kVSb4Lwaib+yBEhnlW4iH9rlpl7LEe080qu+rHrRcaJ4Hzg4TpvMR5 ec2-user@Bastion"
 }
 
 resource "aws_instance" "exmaple" {
-  key_name      = "bastion-key"
+  key_name      = "bastion"
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
 }
